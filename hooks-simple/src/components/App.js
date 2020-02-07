0cbd297c62de242e6b4c44a1d05ccd165899807f
Mark Kimitch <mkimitch@questarai.com>
2020-02-07 12:22:16 -0600

    Completed section on Hooks

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react'
+import ResourceList from './ResourceList'
+import UserList from './UserList'
 
-function App() {
-	return <div>App</div>;
+const App = () => {
+	const [resource, setResource] = useState('posts')
+	return (
+		<div>
+			<UserList />
+			<div>
+				<button onClick={() => setResource('posts')}>Posts</button>
+				<button onClick={() => setResource('todos')}>Todos</button>
+			</div>
+			<ResourceList resource={resource} />
+		</div>
+	)
 }
 
-export default App;
+export default App
